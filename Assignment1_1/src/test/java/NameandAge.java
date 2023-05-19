fffec679751dfbb8dd822df9ff00b2b41583d997
manasibembalkar <manasi.bembalkar@gmail.com>
2023-05-19 15:32:46 +0530

    first comment

---
+++ Assignment1_1/src/test/java/NameandAge.java
@@ -0,0 +1,8 @@
+public class NameandAge {
+    public static void main(String[] args) {
+        String name="kavita";
+        int age=28;
+        System.out.println("Hello "+name);
+        System.out.println("your age is "+age);
+    }
+}
